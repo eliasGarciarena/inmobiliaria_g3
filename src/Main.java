5500377be49ae0bcd81833448ffad2670d8e2a20
eliasGarciarena <102766956+eliasGarciarena@users.noreply.github.com>
2022-07-03 13:21:45 -0300

    test de conexion a db

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@
  */
 import data.Conexion;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -13,7 +17,19 @@ public class Main {
         Conexion intento = new Conexion();
         Connection establecida = intento.getConexion();
         if(establecida != null){
-            System.out.println("Conexion establecida con exito!");
+            try{
+                System.out.println("Conexion establecida con exito!\n\nTABLAS DE LA BD inmobiliaria_g3:\n");
+                String sql = "SHOW TABLES";
+                PreparedStatement ps = establecida.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+                while(rs.next()){
+                    System.out.println("- " + rs.getString(1));
+                }
+            } catch(SQLException ex){
+                JOptionPane.showMessageDialog(null, "ERROR AL EJECUTAR CONSULTA\n" + ex, "Consultar Tablas", JOptionPane.ERROR_MESSAGE);
+                System.out.println("ERROR AL EJECUTAR CONSULTA\n" + ex);
+            }
+            
         } else{
             System.out.println("Upss! Ocurrio un accidente :(");
         }
